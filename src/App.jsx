8326aa0ad317069d92aec6f7161cbaa3380b74a3
Cian O'Gralaigh <ihatecian@gmail.com>
2024-11-16 19:12:50 +0000

    add call to mock api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  return <div>Hello</div>;
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data.users));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {users?.map((user, idx) => (
+          <li key={idx}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,6 +2,11 @@ import { http, HttpResponse } from "msw";
 
 export const handlers = [
   http.get("/api/users", (resolver) => {
-    return HttpResponse.json({ users: [{ id: 1, name: "Frank" }] });
+    return HttpResponse.json({
+      users: [
+        { id: 1, name: "Frank" },
+        { id: 2, name: "Chester" },
+      ],
+    });
   }),
 ];
