cf87682f8af664c1f86a9be9fb0fc25f170fd75f
Cian O'Gralaigh <ihatecian@gmail.com>
2024-11-16 19:49:03 +0000

    add error mock

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -10,6 +10,14 @@ export const handlers = [
     });
   }),
   http.post("/api/messages", async ({ request }) => {
+    const authtoken = request.headers.get("Authorization");
+    if (!authtoken) {
+      return HttpResponse.json(
+        { success: false, message: "Unauthorized" },
+        { status: 401 }
+      );
+    }
+
     const body = await request.json();
     return HttpResponse.json(
       { success: true, content: body.content },
